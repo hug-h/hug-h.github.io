be911969e45de899c5fec9d314416850e74602d5
hug-h <hugh_clery_ward@outlook.com>
2026-06-16 13:27:09 +0100

    shifting liring index to lim3r.ing

--- limRing/onionring-variables.js
+++ limRing/onionring-variables.js
@@ -39,7 +39,7 @@ var ringID = 'limring';
 //should the widget include a link to an index page?
 var useIndex = true;
 //the full URL of the index page. if you're not using one, you don't have to specify anything here
-var indexPage = 'https://hug-h.github.io/limRing/';
+var indexPage = 'https://lim3r.ing';
 
 //should the widget include a random button?
 var useRandom = true;
